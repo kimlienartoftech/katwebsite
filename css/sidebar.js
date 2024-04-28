7dfc88a6b564494d8a6f64a9fdc46af46f870cd6
Thanhaddd <phamducthanh090508@gmail.com>
2024-04-28 16:14:25 +0700

    menubar

--- css/sidebar.js
+++ css/sidebar.js
@@ -5,3 +5,9 @@ function openSide() {
 function closeSide() {
     document.getElementById("sidebar").style.marginLeft = "-300px";
 }
+function openMenu()  {
+  document.getElementById("menubar").style.marginRight= "0";
+}
+function closeMenu()  {
+  document.getElementById("menubar").style.marginRight= "-300px";
+}
